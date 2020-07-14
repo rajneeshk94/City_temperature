8421b3a4a94e19639d64c273618fb38bfed2378d
Rajneesh Khare <rajneeshkhare94@gmail.com>
2020-07-14 17:34:53 +0530

    modified

--- execute.py
+++ execute.py
@@ -1,5 +1,8 @@
 import requests
 import openpyxl
+import sys
+
+run = True 
 
 #Gets temperature of city in F
 def get_temp(city):
@@ -9,6 +12,7 @@ def get_temp(city):
 	temp = city_data['main']['temp']
 	return temp
 
+#Gets humidity data
 def get_humi(city):
 	url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1694fcee8683b88cc0f0dc7845f7087e'
 	res = requests.get(url)
@@ -16,26 +20,36 @@ def get_humi(city):
 	humidity = city_data['main']['humidity']
 	return humidity
 
-#loads workbook and the Weather worksheet
-wb = openpyxl.load_workbook('City_temperatures.xlsx')
-weather = wb['Weather']
-
-#Updates temperature and humidity city wise and unit wise
-for city,unit,temperature,humidity,update in zip(weather['A'], weather['D'], weather['B'], weather['C'], weather['E']):
-	#Skips the first two column
-	if city.value == 'City Name' or city.value == None:
-		continue
-	#Checks Unit and Update value
-	elif unit.value == 'C' and update.value == 1:
-		temperature.value = round((get_temp(city.value) - 32)*(5/9), 0) 
+def update_data():
 	
-	elif unit.value == 'F' and update.value == 1:
-		temperature.value = round(get_temp(city.value), 0)
-	
-	#Checks update value for humidity and updates it
-	if update.value == 1:
-		humidity.value = get_humi(city.value)
-	else:
-		pass
+	#loads workbook and the Weather worksheet
+	wb = openpyxl.load_workbook('City_temperatures.xlsx')
+	weather = wb['Weather']
 
-wb.save('City_temperatures.xlsx')			
+	#Updates temperature and humidity city wise and unit wise
+	for city,unit,temperature,humidity,update in zip(weather['A'], weather['D'], weather['B'], weather['C'], weather['E']):
+		#Skips the first two column
+		if city.value == 'City Name' or city.value == None:
+			continue
+		#Checks Unit and Update value
+		elif unit.value == 'C' and update.value == 1:
+			temperature.value = round((get_temp(city.value) - 32)*(5/9), 0) 
+		
+		elif unit.value == 'F' and update.value == 1:
+			temperature.value = round(get_temp(city.value), 0)
+		
+		#Checks update value for humidity and updates it
+		if update.value == 1:
+			humidity.value = get_humi(city.value)
+		else:
+			pass
+
+	wb.save('City_temperatures.xlsx')			
+	print('Updated..')
+
+try:
+	while run:
+		update_data()
+
+except KeyboardInterrupt:
+	sys.exit()
